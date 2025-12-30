94d7f1d6fac1f3f4967b74883663987b994908a6
try14190-code <try14190@gmail.com>
2025-12-30 15:28:48 +0900

    feat: [이상훈] for 문 활용하여 배열값 합산하기 [PMIF-x]

--- kimjina/javascript/javascript-app/src/App.tsx
+++ kimjina/javascript/javascript-app/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { ControlFlowFor } from './control_flow/ControlFlowFor'
+import { ControlFlowForSummation } from './control_flow/ControlFlowFor copy'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import { ControlFlowSwitch } from './control_flow/ControlFlowIf Switch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
@@ -22,7 +24,8 @@ function App() {
         <ControlFlowIf/>
         <FirstProblem/>
         <ControlFlowSwitch/>
-        
+        <ControlFlowFor/>
+        <ControlFlowForSummation/>
       </div>
       
     </>
